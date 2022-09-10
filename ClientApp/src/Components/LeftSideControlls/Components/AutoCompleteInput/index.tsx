83afee76cc513f95be978201eb55a5da164a9fb0
XanEver <68378702+XanEver@users.noreply.github.com>
2022-09-10 15:56:30 +0300

    fix map

--- ClientApp/src/Components/LeftSideControlls/Components/AutoCompleteInput/index.tsx
+++ ClientApp/src/Components/LeftSideControlls/Components/AutoCompleteInput/index.tsx
@@ -1,5 +1,5 @@
 import { ReactNode, FC, useState, useEffect } from 'react'
-import { AutoComplete, Input } from 'antd'
+import { AutoComplete, Input, Select } from 'antd'
 
 type AutoCompleteInputT = {
     prefixInput?:ReactNode
@@ -9,10 +9,16 @@ type AutoCompleteInputT = {
     onChange?:(value:string) => any
     onSelected?:(option:{label:string,value:[number,number]}) => any
 }
+const { Option } = Select
 
 const AutoCompleteInput:FC<AutoCompleteInputT> = ({ prefixInput,placeholder,onChange,options,defVal,onSelected }) => {
 
     const [ v, setV ] = useState<Array<{key:string | number,label:string,value:[number,number]}> | undefined>(undefined)
+    const [ inputValue, setInputValue ] = useState<string>("");
+
+  useEffect(() => {
+    console.log(inputValue);
+  },[inputValue])
 
     useEffect(() => {
         console.log(options);
@@ -23,22 +29,50 @@ const AutoCompleteInput:FC<AutoCompleteInputT> = ({ prefixInput,placeholder,onCh
     },[options])
 
   return (
-    <AutoComplete 
-        bordered={false}
-        onSearch={(e:string) => {
-            console.log(e)
-            if(e.length >= 3){
-                onChange && onChange(e)
-            }
-        }}
-        options={v}
-        onSelect={(value:any,option:{label:string,value:[number,number]}) => {
-            console.log(option);
-            onSelected && onSelected(option)
-        }}
+    // <AutoComplete 
+    //     bordered={false}
+    //     onSearch={(e:string) => {
+    //         if(e.length >= 3){
+    //             onChange && onChange(e)
+    //         }
+    //     }}
+    //     options={v}
+    // >
+    //     <Input 
+    //         className='h-10 border-0.5 rounded-lg border-b-2 border-zinc-200 hover:border-lime-300 ' 
+    //         placeholder={placeholder} 
+    //         prefix={prefixInput}
+    //         value={inputValue}
+    //         onChange={( { target:{ value } } ) => setInputValue(value)}
+    //     />
+    // </AutoComplete>
+    <Select
+      onSearch={(value) => {
+        if(value.length >= 3){
+          onChange && onChange(value)
+        }
+      }}
+      value={v}
+      
+      placeholder={placeholder}
+      suffixIcon={prefixInput}
+      defaultActiveFirstOption={false}
+      filterOption={false}
+      showSearch
+      showArrow={false}
+      notFoundContent={null}
+      onSelect={(value:any,option:any) => {
+        console.log(option);
+      }}
     >
-        
-    </AutoComplete>
+      {
+        v?.map((opt) => {
+          return (
+            <Option value={opt.value}>{opt.label}</Option>
+          )
+        })
+      }
+    </Select>
   )
 }
 
